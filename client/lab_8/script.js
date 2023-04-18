a48bdb738293d61c5ef4cf1597f9953e32d1a615
LaMich25 <109875989+LaMich25@users.noreply.github.com>
2023-04-18 15:26:27 -0400

    map, markers

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -44,10 +44,35 @@ function cutRestaurantList(list) {
 
 }
 
+function initMap() {
+    const carto = L.map('map').setView([38.98, -76.93], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(carto);
+    return carto;
+}
+
+function markerPlace(array, map) {
+    console.log('array for markers:', array);
+    map.eachLayer((layer) => {
+        if (layer instanceof L.Marker) {
+            layer.remove();
+        }
+    });
+
+    array.forEach((item) => {
+        console.log('markerplace', item);
+        const { coordinates } = item.geocoded_column_1;
+        L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    })
+}
 async function mainEvent() { // the async keyword means we can make API requests
     const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
     // Add a querySelector that targets your filter button here
+    // AdclearquerySelector that targets your filter buttclearere
     const loadDataButton = document.querySelector('#data_load');
+    const clearDataButton = document.querySelector('#data_clear');
     const generateListButton = document.querySelector('#generate');
     const textField = document.querySelector('#resto');
 
@@ -55,10 +80,11 @@ async function mainEvent() { // the async keyword means we can make API requests
     loadAnimation.style.display = 'none';
     generateListButton.classList.add('hidden');
 
-    //let storedList = [];
+    const carto = initMap();
+
     const storedData = localStorage.getItem('storedData');
-    const parsedData = JSON.parse(storedData);
-    if(parsedData.length > 0){
+    let parsedData = JSON.parse(storedData);
+    if (parsedData?.length > 0) {
         generateListButton.classList.remove('hidden');
     }
 
@@ -110,10 +136,14 @@ async function mainEvent() { // the async keyword means we can make API requests
         loadAnimation.style.display = 'inline-block'
         const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
         const storedList = await results.json();
-        localStorage.setItem('storedData', JSON.stringify(storedList))
+        localStorage.setItem('storedData', JSON.stringify(storedList));
+        parsedData = storedList;
 
-        
-        
+        if (storedList?.length>0){
+            generateListButton.classList.remove("hidden");
+        }
+
+
         loadAnimation.style.display = 'none';
         console.table(storedList);
     })
@@ -126,6 +156,7 @@ async function mainEvent() { // the async keyword means we can make API requests
         currentList = cutRestaurantList(parsedData);
         console.log(currentList);
         injectHTML(currentList);
+        markerPlace(currentList, carto);
     })
 
     textField.addEventListener("input", (event) => {
@@ -133,8 +164,14 @@ async function mainEvent() { // the async keyword means we can make API requests
         const newList = filterList(currentList, event.target.value);
         console.log(newList);
         injectHTML(newList);
+        markerPlace(newList, carto);
     })
 
+    clearDataButton.addEventListener("click", (event) => {
+        console.log('clear browser data');
+        localStorage.clear();
+        console.log('localStorage Check', localStorage.getItem("storedData"))
+    })
 }
 
 /*
